a3b297df04e8658983a2eb934b0b53cde3290bf2
sharankasandula <sharankasandula@gmail.com>
2019-01-12 05:32:27 +0100

    prettifying dashboard using bootstrap and CSS

--- src/app/customers/customers-list/filter-textboxcomponent/filter-textboxcomponent.component.ts
+++ src/app/customers/customers-list/filter-textboxcomponent/filter-textboxcomponent.component.ts
@@ -3,8 +3,12 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-filter-textboxcomponent',
   template: `
-        Filter: <input type="text" [(ngModel)]="filter" />
-    `
+  <div class="row">
+    <div class="col offset-1 offset-sm-3 offset-md-4 offset-lg-7 offset-xl-8">
+      <input type="search" placeholder="Search" [(ngModel)]="filter" autofocus class=" border rounded  form-control" />
+    </div>
+  </div>
+      `
 })
 export class FilterTextboxcomponentComponent implements OnInit {
 
